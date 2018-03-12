e6d1cf39456ce205e1aa7d5670bed3d489a0bf3a
Robert Rozas Navarro <robert.rozas.n@gmail.com>
2018-03-12 13:09:34 -0300

    Se añaden demo por consola para correr los test contra el ejecutable y generar un reporte html

--- ConsoleBDDSpecFlow/ConsoleBDDSpecFlow/Calculadora.cs
+++ ConsoleBDDSpecFlow/ConsoleBDDSpecFlow/Calculadora.cs
@@ -6,13 +6,16 @@ using System.Threading.Tasks;
 
 namespace ConsoleBDDSpecFlow
 {
+    /*
+     nunit3-console.exe --labels=All --out=TestResult.txt "--result=TestResult.xml;format=nunit2" bin\Debug\ConsoleBDDSpecFlow.exe
+     specflow.exe nunitexecutionreport ConsoleBDDSpecFlow.csproj /out:MyResult.html    */
     class Calculadora
     {
         public int primerNumero { get; set; }
         public int segundoNumero { get; set; }
         static void Main(string[] args)
         {
-
+            throw new NotSupportedException();
         }
 
         public int Sumar()
