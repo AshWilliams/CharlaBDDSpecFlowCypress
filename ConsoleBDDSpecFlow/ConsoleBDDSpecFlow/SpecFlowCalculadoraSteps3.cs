ee71cf1212ff4bc6472a01da031d5c108c3ad925
Robert Rozas Navarro <rorozasn@microsoft.com>
2018-10-19 00:37:38 -0500

    ultimas mejoras a la presentación

---
+++ ConsoleBDDSpecFlow/ConsoleBDDSpecFlow/SpecFlowCalculadoraSteps3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace ConsoleBDDSpecFlow
+{
+    [Binding]
+    public class SpecFlowCalculadoraSteps3
+    {
+        [Given(@"Ingresen (.*) a la Calculadora")]
+        public void GivenIngresenALaCalculadora(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"Y Ingresen (.*) a la calculadora")]
+        public void GivenYIngresenALaCalculadora(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Cuando llamamos a Multiplicar")]
+        public void WhenCuandoLlamamosAMultiplicar()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"El resultado de la multiplicacion debiese ser (.*)")]
+        public void ThenElResultadoDeLaMultiplicacionDebieseSer(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
